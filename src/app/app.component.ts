46e64ea1db8280318fdee7ade508965c401d1299
ljazminsc205 <ljazminsc205@gmail.com>
2021-01-11 06:26:38 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'my-app';
+  rutas = [{ name: 'Card', ruta: 'https://card-flip-30aba.web.app/' },
+    { name: 'Portafolio', ruta: 'https://mi-portafolio-web.web.app/' },
+    { name: 'Back', ruta: 'https://background-float.web.app/' },
+    { name: 'Notas', ruta: 'https://notas-f5bbc.web.app/home' },
+    { name: 'Cronometro', ruta: 'https://cronometro-8fe15.web.app/home' }
+  ]
 }
