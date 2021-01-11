00a51ad64080949dd3c0e6621210780a4537f295
Lery Sanchez <ljazminsc205@gmail.com>
2021-01-11 17:41:25 -0600

    my third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   rutas = [ 
-      { name: 'Notas', ruta: 'https://notas-f5bbc.web.app/home' , img:'../assets/notas.png', description:'' },
-      { name: 'Cronómetro', ruta: 'https://cronometro-8fe15.web.app/home', img:'../assets/cronometro.png', description:'' },
-      { name: 'Bizquick', ruta: 'https://ehs-institute.web.app/' , img:'../assets/renase.png' ,description:'' }
-   ]
+    { name: 'Bizquick', ruta: 'https://ehs-institute.web.app/' , img:'../assets/renase.png' ,description:'' },
+    { name: 'Curso Introducción al Desarrollo de páginas Web', ruta: 'https://mi-portafolio-web.web.app/', img:'../assets/miportafolio.png', description:'' },
+    { name: 'Cronómetro', ruta: 'https://cronometro-8fe15.web.app/home', img:'../assets/cronometro.png', description:'' },
+  ]
   rutas2=[
+    { name: 'Notas', ruta: 'https://notas-f5bbc.web.app/home' , img:'../assets/notas.png', description:'' },
       { name: 'Cards', ruta: 'https://card-flip-30aba.web.app/', img:'../assets/cards.png' ,description:'' },
-      { name: 'Curso Introducción al Desarrollo de páginas Web', ruta: 'https://mi-portafolio-web.web.app/', img:'../assets/miportafolio.png', description:'' },
       { name: 'Background flotante', ruta: 'https://background-float.web.app/' ,img:'../assets/back.png',description:'' }
     ]
   
